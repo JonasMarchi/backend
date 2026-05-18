bda5b8def3a6c19d46b54be676ae57ca5b075f78
Jonas Marchi Lemes da Silva <jonasmarchi12@gmail.com>
2026-05-18 10:30:08 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,87 @@
+// Projeto básico de rotas - backend com node.js
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+//banco de dados fake
+let usuarios = [
+    {id: 1, nome: "João", email: "joao@gmail.com"},
+    {id: 2, nome: "Maria", email: "maria@gmail.com"},
+    {id: 3, nome: "Carlos", email: "carlos@gmail.com"},
+];
+
+//get - Pagina inicial
+app.get("/", (req, res) =>{
+    res.send("Servidor funcionando com sucesso!");
+});
+
+//get - Lista de usuarios
+app.get("/usuarios", (req, res) =>{
+    res.json(usuarios);
+});
+
+//get com parametro("filtro")
+app.get("usuario/:id", (req, res) =>{
+    const id = Number(req.params.id);
+
+    const usuario = usuarios.find(u => u.id === id);
+    if(!usuario){
+        return res.status(404).json({
+            mensagem: "Usuário não encontrado"
+        });
+    }
+    res.json(usuario);
+});
+
+//post - cadastrar usuario
+app.post("/cadastro", (req, res) =>{
+    const{ nome, email } = req.body;
+
+    if(!nome || !email){
+        return res.status(400).json({
+            mensagem: "Nome e email são obrigatórios"
+        });
+    }
+    const novoUsuario = {
+        id:usuarios.length + 1,
+        nome,
+        email
+    };
+
+    usuarios.push(novoUsuario);
+
+    res.status(201).json({
+        mensagem: "Cadastro realizado com sucesso!",
+        usuario: novoUsuario
+    });
+})
+
+//Put - editar usuario
+app.put("/editar/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const{nome, email } = req.body;
+    const usuario = usuarios.find(u => u.id === id);
+    if(!usuario){
+        return res.status(404).json({
+            mensagem: "Usuário não encontrado"
+        });
+    }
+    req.json({
+        mensagem: "Usuário editado com sucesso!",
+        usuario
+    })
+});
+
+//Delete - deletar usuario
+
+app.delete("deletar/id:", (req, res) => {
+    const id = Number(req.params.id);
+    const index = usuarios.findIndex(u => u.id === id);
+    if(index === -1 ){
+        return res.status(404).json({
+            mensagem: "Usuário não encontrado"
+        });
+    }
+    
+})
